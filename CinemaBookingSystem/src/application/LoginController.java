ecf7c1651db0c261900294da9fad588780aff333
Aleksi Anttila <aleksi.anttila303@gmail.com>
2017-11-01 16:11:01 +0000

    Add login scene Add controller for login scene

---
+++ CinemaBookingSystem/src/application/LoginController.java
@@ -0,0 +1,17 @@
+package application;
+
+//import java.util.Random;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+	//@FXML
+	//private Label myMessage;
+	public void generateRandom(ActionEvent event) {
+		//Random random = new Random();
+		//int myRand = random.nextInt(50) + 1;
+		//myMessage.setText(Integer.toString(myRand));
+	}
+}
