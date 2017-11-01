f5b41bc4d5c84a7659d3c7a48a40ce66953b2e56
Zaid Al Lahham <zlahham@gmail.com>
2017-11-01 16:44:55 +0000

    Add LoginController#validateCredentials - implement basic authentication

--- CinemaBookingSystem/src/application/LoginController.java
+++ CinemaBookingSystem/src/application/LoginController.java
@@ -1,17 +1,33 @@
 package application;
 
-//import java.util.Random;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-	//@FXML
-	//private Label myMessage;
-	public void generateRandom(ActionEvent event) {
-		//Random random = new Random();
-		//int myRand = random.nextInt(50) + 1;
-		//myMessage.setText(Integer.toString(myRand));
+	@FXML private Label lblTest;
+	@FXML private TextField txtUsername;
+	@FXML private TextField txtPassword;
+	
+	public void validateCredentials(ActionEvent event) {
+		validateUsername(txtUsername.getText());
+		validatePassword(txtPassword.getText());
+	}
+	
+	private void validateUsername(String username) {
+		if(username.compareTo("username") == 0) {
+			lblTest.setText("Success!");
+		} else {
+			lblTest.setText("Try Again!");
+		}
+	}
+	
+	private void validatePassword(String password) {
+		if(password.compareTo("password") == 0) {
+			lblTest.setText("Success!");
+		} else {
+			lblTest.setText("Try Again!");
+		}
 	}
 }
