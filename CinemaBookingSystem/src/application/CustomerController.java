0d91a302584767a80cfc0c85bbd4e10ccdd4fa05
Aleksi Anttila <aleksi.anttila303@gmail.com>
2017-11-08 16:06:29 +0000

    Add CustomerController class; Add buttons and labels to customer view

---
+++ CinemaBookingSystem/src/application/CustomerController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class CustomerController {
+	public void viewBookings(ActionEvent event) {
+		System.out.println(Main.user.getUsername());
+	}
+	public void newBooking() {
+	}
+	public void changeDetails() {
+	}
+	
+}
