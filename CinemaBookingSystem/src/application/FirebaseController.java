5b2ac6dd1dd6c5fce51b4d0b9c8f29155993a81d
Zaid Al Lahham <zlahham@gmail.com>
2017-11-29 15:16:55 +0000

    Return JSON object from the get method

--- CinemaBookingSystem/src/application/FirebaseController.java
+++ CinemaBookingSystem/src/application/FirebaseController.java
@@ -1,14 +1,17 @@
 package application;
 
+import org.json.JSONObject;
+
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class FirebaseController {
 	private static String URL = "https://cinema-booking-app.firebaseio.com/";
-
-	public static void get(String className, String id) throws UnirestException {
+	
+	public static JSONObject getList(String className) throws UnirestException {
 		String response = Unirest.get(URL + className + ".json").asString().getBody();
-		System.out.println(response);
+		JSONObject listJSON = new JSONObject(response);
+		return listJSON;
 	}
 
 	public static boolean post() {
