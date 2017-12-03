93725308b5a9fadf73ae7b7a31b500570adfe24b
Zaid Al Lahham <zlahham@gmail.com>
2017-12-03 18:10:05 +0000

    Create User from registration page and add styling

--- CinemaBookingSystem/src/application/LoginController.java
+++ CinemaBookingSystem/src/application/LoginController.java
@@ -1,9 +1,8 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,30 +12,51 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import application.sevices.Firebase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController extends MainController {
 	@FXML
-	private Label lblTest;
+	private Label lblSuccess;
+	@FXML
+	private Label lblFailure;
+	@FXML
+	private Label lblEmail;
+	@FXML
+	private Label lblUsername;
+	@FXML
+	private Label lblPassword;
+	@FXML
+	private Label lblFirstname;
+	@FXML
+	private Label lblLastname;
+
+	@FXML
+	private TextField txtEmail;
 	@FXML
 	private TextField txtUsername;
 	@FXML
-	private TextField txtPassword;
+	private TextField txtFirstname;
+	@FXML
+	private TextField txtLastname;
+
 	@FXML
 	private PasswordField pwPassword;
 
 	public void validateCredentials(ActionEvent event) throws UnirestException {
-		
+
 		if (txtUsername.getText().equals("") || pwPassword.getText().equals("")) {
-			lblTest.setText("Username/Password cannot be empty");
+			lblFailure.setText("Username/Password cannot be empty");
 			return;
 		} else {
 			JSONObject userJSON = findUser(txtUsername.getText(), pwPassword.getText());
 
 			if (userJSON != null) {
-				lblTest.setText("Success");
+				lblSuccess.setText("Success");
 
 				User user = null;
 
@@ -49,17 +69,53 @@ public class LoginController extends MainController {
 				transitionToUserView(user);
 
 			} else {
-				lblTest.setText("Failure");
+				lblFailure.setText("Failure");
 			}
 
 		}
 
 	}
 
-	// TODO: Sort this out
-	public void createUser(ActionEvent event) {
-		System.out.println("kill me");
-		String content = null;
+	public void transitionToRegistrationView(ActionEvent event) {
+
+		try {
+			Parent registrationView;
+
+			registrationView = FXMLLoader.load(getClass().getResource("/application/Registration.fxml"));
+			Scene scene = new Scene(registrationView, 750, 500);
+			Main.stage.setScene(scene);
+			Main.stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void createUser(ActionEvent event) throws UnirestException {
+		lblFailure.setText("");
+		lblSuccess.setText("");
+
+		if (txtEmail.getText().equals("") || pwPassword.getText().equals("") || txtUsername.getText().equals("")
+				|| txtFirstname.getText().equals("") || txtLastname.getText().equals("")) {
+			lblFailure.setText("Please fill in the entire form!");
+			return;
+		} else {
+			try {
+				Firebase.getItem("users", txtUsername.getText());
+			} catch (JSONException e) {
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("username", txtUsername.getText());
+				params.put("email", txtEmail.getText());
+				params.put("firstname", txtFirstname.getText());
+				params.put("lastname", txtLastname.getText());
+				params.put("password", pwPassword.getText());
+
+				Firebase.createItem("users", params);
+				lblSuccess.setText("User created, please login!");
+				return;
+			}
+
+			lblFailure.setText("Username is taken, please choose another one");
+		}
 
 	}
 
