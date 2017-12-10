9788a24d5d516d52f37bfb52314b26fbd0f6175d
Zaid Al Lahham <zlahham@gmail.com>
2017-12-10 13:43:00 +0000

    Add Export film list feature

--- src/main/java/application/controllers/EmployeeController.java
+++ src/main/java/application/controllers/EmployeeController.java
@@ -1,7 +1,62 @@
 package application.controllers;
 
+import application.Main;
+import application.models.Film;
+import application.models.Screening;
+import javafx.collections.ObservableList;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class EmployeeController extends MainController {
 
-	public void exportFilms() {
-	}
+    public void exportFilms() {
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("filmsExportList.txt"))) {
+            bw.write("AZ CINEMAS FILM LIST DETAILS:");
+            bw.newLine();
+            bw.write("=============================");
+            bw.newLine();
+
+            for (Film f : Main.filmList) {
+                bw.write("Title: " + f.getFilmTitle());
+                bw.newLine();
+                bw.write("Description: " + f.getDescription());
+                bw.newLine();
+                bw.write("Age Rating : " + f.getAgeRating());
+                bw.newLine();
+
+                ObservableList<Screening> screenings = f.getScreenings();
+                for (int i = 0; i < screenings.size(); i++) {
+                    Screening s = screenings.get(i);
+                    int bookedSeats = 0;
+                    int availableSeats = 0;
+
+                    bw.write("Screening #" + (i + 1) + ": " + s.getDateTime());
+                    bw.newLine();
+                    for(Boolean value: s.getSeats().values()) {
+                        if (value.equals(true)) {
+                            bookedSeats++;
+                        }else{
+                            availableSeats++;
+                        }
+                    }
+                    bw.write("\tAvailable Seats: " + availableSeats);
+                    bw.newLine();
+                    bw.write("\tBooked Seats: " + bookedSeats);
+                    bw.newLine();
+                    bw.write("\n");
+                }
+
+                bw.newLine();
+                bw.write("----------------------------------------------");
+                bw.newLine();
+                bw.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
 }
